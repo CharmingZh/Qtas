6ef6ed3bc4b506d353f1c991a094a18ecca5f15a
Jiaming_Zhang <jm.zhang.1997@outlook.com>
2021-11-05 01:26:27 +0800

    A basic structure of file system is emerging!!

--- samples/Vfile.py
+++ samples/Vfile.py
@@ -1,3 +1,29 @@
+import os
+
 class File:
-    print()
+    def __init__(self, Name):
+        self.file = open(Name, 'r')
+        self.name = Name
+        self.size = os.path.getsize(self.name)
+
+    def __del__(self):
+        self.file.close()
+
+    def read_file(self):
+        print("read file")
+        data = "Info in the file"
+        return data
+        # to do
+
+    def write_file(self, info):
+        print("Write file")
+        # to do
+
+    # to do
+
+class Dir:
+    def __init__(self):
+        print()
+        # to do
+
     # to do
