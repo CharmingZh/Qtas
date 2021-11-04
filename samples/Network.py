5c45aeb0e124cdcfb0d7917a6057b4e5e4b948e6
Jiaming_Zhang <jm.zhang.1997@outlook.com>
2021-11-05 01:25:35 +0800

    A rudimentary Server/Client implemented

--- samples/Network.py
+++ samples/Network.py
@@ -1,7 +1,30 @@
-import socket
+import time
+from socket import *
+import threading
+import random
 
 
-def get_ip(version = 4):
+# import Cepher
+# from settings import SIZE
+
+
+
+def gen_port(num=1):
+    """
+        Generate a port number or a list
+    """
+    ports_list = [random.randrange(10000, 60000) for x in range(num)]
+    if num == 1:
+        # s.bind((Network.get_ip(4), 0)) is enough
+        return ports_list[0]
+    else:
+        return ports_list
+
+
+def get_ip(version=4):
+    """
+        Get the IP addr of the client
+    """
     if version == 4:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect(('1.1.1.1', 80))
@@ -17,14 +40,73 @@ def get_ip(version = 4):
     else:
         print("version type wrong")
 
-class Server:
-    print()
+
+class Server(threading.Thread):
+    def __init__(self, port, version):
+        threading.Thread.__init__(self)
+        self.setDaemon(True)
+        self.address = (get_ip(version), port)
+        if version == 4:
+            self.sock = socket(AF_INET, SOCK_STREAM)
+        elif version == 6:
+            self.sock = socket(AF_INET6, SOCK_STREAM)
+
+    def __del__(self):
+        self.sock.close()
+        # to do
+
+    def run(self):
+        print("server start ...")
+        self.sock.bind(self.address)
+        self.sock.listen(1)
+        conn, addr = self.sock.accept()
+        print("remote client success connected ...")
+        # to do
+
+
+class Client(threading.Thread):
+    def __init__(self, port, version):
+        threading.Thread.__init__(self)
+        self.setDaemon(True)
+        self.address = (get_ip(version), port)
+        if version == 4:
+            self.sock = socket(AF_INET, SOCK_STREAM)
+        elif version == 6:
+            self.sock = socket(AF_INET6, SOCK_STREAM)
+        # to do
+
+    def __del__(self):
+        self.sock.close()
+        # to do
+
+    def run(self):
+        print("client start ...")
+        while True:
+            try:
+                self.sock.connect(self.address)
+                break
+            except:
+                # 连接失败时，三秒后重新尝试连接
+                count = 3
+                while count >= 0:
+                    print("      reconnect in ", count, "seconds")
+                    time.sleep(1)
+                    count -= 1
+                    continue
+        print("client success connected")
+        timer = 100
+        while timer >= 0:
+            print("test", timer, "seconds")
+            timer -= 1
+            time.sleep(1)
+
+        # to do
     # to do
 
-class Client:
-    print()
-    # to do
 
 if __name__ == '__main__':
     ip = get_ip()
     print(ip)
+    print("gen ports")
+    print(gen_port(1))
+    print(gen_port(10))
